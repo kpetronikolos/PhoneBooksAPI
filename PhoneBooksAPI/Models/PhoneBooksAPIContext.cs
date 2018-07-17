16706255d7e053a970da43c7d0ebe523d50caea1
konstantinos <kpetronikolos@gmail.com>
2018-07-17 22:28:12 +0300

    Set up Entity Framework Core.

---
+++ PhoneBooksAPI/Models/PhoneBooksAPIContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneBooksAPI.Models
+{
+    public class PhoneBooksAPIContext : DbContext
+    {
+        public PhoneBooksAPIContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Phone> Phones { get; set; }
+    }
+}
